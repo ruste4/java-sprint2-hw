4cb1c621d5c7559b936873356605de4f57610b0b
ruste4 <te4rus@yandex.ru>
2022-03-30 03:23:10 +0500

    feat: добавить KVTaskClient

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import generators.TaskGenerator;
 import server.HttpTaskServer;
 import server.KVServer;
+import server.KVTaskClient;
 import tasks.EpicTask;
 import tasks.MonoTask;
 import tasks.Subtask;
@@ -11,7 +12,19 @@ public class Main {
     private static HttpTaskServer server = new HttpTaskServer();
 
     public static void main(String[] args) throws IOException {
+        String json = "{\n" +
+                "\t\"epicID\": 2,\n" +
+                "\t\"title\": \"Sub Task2\",\n" +
+                "\t\"description\": \"Description sub task3\",\n" +
+                "\t\"id\": 7,\n" +
+                "\t\"status\": \"DONE\",\n" +
+                "\t\"type\": \"SUBTASK\",\n" +
+                "\t\"duration\": 0\n" +
+                "}";
         KVServer kvServer = new KVServer();
         kvServer.start();
+        KVTaskClient kvTaskClient = new KVTaskClient("http://localhost:8078/");
+        kvTaskClient.put("id7", json);
+        System.out.println(kvTaskClient.load("id7"));
     }
 }
---
+++ src/server/KVTaskClient.java
@@ -0,0 +1,64 @@
+package server;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class KVTaskClient {
+    HttpClient client;
+    private String url;
+    private String keyApi;
+
+    public KVTaskClient(String url) {
+        this.client = HttpClient.newHttpClient();
+        this.url = url;
+        this.keyApi = registration();
+    }
+
+    private String registration() {
+        URI uri = URI.create(url + "register/");
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(uri)
+                .version(HttpClient.Version.HTTP_1_1)
+                .build();
+
+        return sendRequest(request).body();
+    }
+
+    public void put(String key, String json) {
+        URI uri = URI.create(url + "save/" + key + "?API_KEY=" + keyApi);
+        HttpRequest.BodyPublisher body = HttpRequest.BodyPublishers.ofString(json);
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(body)
+                .uri(uri)
+                .version(HttpClient.Version.HTTP_1_1)
+                .header("Accept", "application/json")
+                .build();
+
+        sendRequest(request);
+    }
+
+    public String load(String key) {
+        URI uri = URI.create(url + "load/" + key + "?API_KEY=" + keyApi);
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(uri)
+                .version(HttpClient.Version.HTTP_1_1)
+                .build();
+        return sendRequest(request).body();
+    }
+
+    private HttpResponse<String> sendRequest(HttpRequest request) {
+        HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
+
+        try {
+            return client.send(request, handler);
+        } catch (IOException | InterruptedException e) {
+            e.getStackTrace();
+            return null;
+        }
+    }
+}
